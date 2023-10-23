c6c9b54317d728a109bbb9d56c8182f2cd7ecf99
anish <anish@LAPTOP-MNPUSC8T>
2023-10-24 10:26:28 +1100

    Add project files.

---
+++ LostandFoundApp/Start.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace LostandFoundApp
+{
+    partial class Start
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.register = new System.Windows.Forms.Button();
+            this.login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // register
+            // 
+            this.register.Location = new System.Drawing.Point(181, 214);
+            this.register.Name = "register";
+            this.register.Size = new System.Drawing.Size(93, 40);
+            this.register.TabIndex = 0;
+            this.register.Text = "Register";
+            this.register.UseVisualStyleBackColor = true;
+            this.register.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(477, 217);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(86, 37);
+            this.login.TabIndex = 1;
+            this.login.Text = "Login";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Start
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.register);
+            this.Name = "Start";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button register;
+        private System.Windows.Forms.Button login;
+    }
+}
+
